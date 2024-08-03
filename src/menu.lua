bdcd99713bae408594d4683dcf6151db5de16c52
almalinux Cloud User <dupontstef80@gmail.com>
2024-08-03 20:45:57 +0000

    refactor(navigation): recoloured

--- src/menu.lua
+++ src/menu.lua
@@ -10,22 +10,38 @@ local myMenu = {
     }
 }
 
-local defaultColor = {r = 0, g = 150, b = 255, a = 255}
-local selectedColor = {r = 255, g = 0, b = 0, a = 200}
+local defaultColor = {r = 75, g = 75, b = 75, a = 220}
+local selectedColor = {r = 255, g = 255, b = 255, a = 200}
+
+local defaultTextColor = {r = 255, g = 255, b = 255, a = 255}
+local selectedTextColor = {r = 0, g = 0, b = 0, a = 255}
 
 function DrawMenu()
-    DrawRect(0.8, 0.4, 0.2, 0.5, 175, 145, 210, 255)
+    -- Frame
+    DrawRect(0.8, 0.4, 0.2, 0.5, 42, 42, 42, 150)
 
-    local count = 0.2
+    --title
+    SetTextEntry("STRING")
+    AddTextComponentString(myMenu.title)
+    SetTextScale(0.0*1, 0.55*1)
+    SetTextFont(0)
+    DrawText(0.7, 0.15)
+
+    -- Items
+    local count = 0.22
     for i, items in ipairs(myMenu['items']) do
         local color = (i-1 == currentItem) and selectedColor or defaultColor
+        local textColor =  (i-1 == currentItem) and selectedTextColor or defaultTextColor
+
         SetTextEntry("STRING")
         AddTextComponentString(items.name)
         SetTextScale(0.0*1, 0.55*1)
         SetTextFont(0)
+        SetTextColour(textColor.r, textColor.g, textColor.b, textColor.a)
+
         DrawText(0.7, count - 0.02)
         DrawRect(0.8, count, 0.2, 0.05, color.r, color.g, color.b, color.a)
-        count = count + 0.075
+        count = count + 0.055
     end
 end
 
